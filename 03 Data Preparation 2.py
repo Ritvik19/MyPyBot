5e868b68bf73731dd6bc9f78408a38ca7b5edf2e
Ritvik19 <rastogiritvik99@gmail.com>
2019-08-13 01:37:53 +0530

    Data Preparation

--- 03 Data Preparation 2.py
+++ 03 Data Preparation 2.py
@@ -4,38 +4,53 @@ from tqdm.auto import tqdm
 import re
 import pickle
 from keras.preprocessing.text import Tokenizer
+import time, datetime
+
+def timestamp():
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S:')
+    return st
 tqdm.pandas()
-print('Libraries Imported')
+
+print(timestamp(), 'Libraries Imported')
 
 
 tokens = pd.read_csv('E:/Scrapped-Data/blogs/posts/tokens.csv')['tokens'].progress_apply(lambda x:str(x).lower()).values.tolist()
-print('File Read', len(tokens))
+print(timestamp(), 'File Read', len(tokens))
 
-tokenizer = Tokenizer()
-print('Tokenizer Instantiated')
+# tokenizer = Tokenizer()
+# print(timestamp(), 'Tokenizer Instantiated')
 
-tokenizer.fit_on_texts(tokens)
-print('Tokenizer Trained')
+# tokenizer.fit_on_texts(tokens)
+# print(timestamp(), 'Tokenizer Trained')
 
-pickle.dump(tokenizer, open('E:/Tokenizer.pkl', 'wb'))
-print('Tokenizer Saved')
+# pickle.dump(tokenizer, open('E:/Tokenizer.pkl', 'wb'))
+# print(timestamp(), 'Tokenizer Saved')
 
-tokens = tokenizer.texts_to_sequences(tokens)
-print('Indexed', len(tokens))
+tokenizer = pickle.load(open('E:/Tokenizer.pkl', 'rb'))
+print(timestamp(), 'Tokenizer Instantiated')
 
-tokens = [item for sublist in tokens for item in tqdm(sublist)]
-print('Flattened', len(tokens))
+tokens = tokenizer.texts_to_sequences(tqdm(tokens))
+print(timestamp(), 'Indexed', len(tokens))
 
+pickle.dump(tokens, open('E:/Tokens.pkl', 'wb'))
+print(timestamp(), 'Pickled')
 
-train_len = 3 + 1
-sequences = []
+tokens = [item for sublist in tqdm(tokens) for item in sublist]
+print(timestamp(), 'Flattened', len(tokens))
 
-for i in tqdm(range(train_len, len(tokens)+1)):
-    seq = tokens[i-train_len:i]
+pd.DataFrame({'tokens':tokens}).to_csv('E:/Scrapped-Data/blogs/posts/token_ids.csv', index=False)
+print(timestamp(), 'File Saved')
+
+# train_len = 3 + 1
+# sequences = []
+
+# for i in tqdm(range(train_len, len(tokens)+1)):
+#     seq = tokens[i-train_len:i]
     
-    sequences.append(seq)
+#     sequences.append(seq)
 
-print('Sequenced', len(sequences))
+# print('Sequenced', len(sequences))
 
-pd.DataFrame(sequences).to_csv('E:/Scrapped-Data/blogs/posts/sequences.csv', index=False)
-print('File Saved')
+# pd.DataFrame(sequences).to_csv('E:/Scrapped-Data/blogs/posts/sequences.csv', index=False)
+# print('File Saved')
